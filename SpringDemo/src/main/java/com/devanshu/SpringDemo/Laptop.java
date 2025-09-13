e12af46644be157e0cc0bd28ff4cf64897a0e1bd
Devanshu Jatav <devanshujatav2003@gmail.com>
2025-09-13 07:49:00 +0530

    Sprint Boot Demo

---
+++ SpringDemo/src/main/java/com/devanshu/SpringDemo/Laptop.java
@@ -0,0 +1,10 @@
+package com.devanshu.SpringDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling...");
+    }
+}
---
+++ SpringDemo/src/main/java/com/devanshu/SpringDemo/SpringDemoApplication.java
@@ -0,0 +1,19 @@
+package com.devanshu.SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
+
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
+
+    }
+
+}
