ec6a16ee85e85e5bfccc06ad7e715bf800d12093
Devanshu Jatav <devanshujatav2003@gmail.com>
2025-10-04 21:02:09 +0530

    SpringSecurity

---
+++ springSecurity/src/main/java/com/example/devanshu/AppSecurityConfig.java
@@ -0,0 +1,85 @@
+package com.example.devanshu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class AppSecurityConfig extends WebSecurityConfiguration {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	
+//	// Using Normal Password Encoder
+//	public AuthenticationProvider authProvider() {
+//		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(userDetailsService);
+//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//		return provider;
+//	}
+	
+	
+	// Using Bycrypt Password 
+//	public AuthenticationProvider authProvider() {
+//		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(userDetailsService);
+//		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//		return provider;
+//	}
+//	
+//	
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http 
+//			.csrf().disable()
+//			.authorizeRequests().requestMatchers("/login").permitAll()
+//			.anyRequest().authenticated()
+//			.and()
+//			.formLogin()
+//			.loginPage("/login").permitAll()
+//			.and()
+//			.logout().invalidateHttpSession(true)
+//			.clearAuthentication(true)
+//			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//			.logoutSuccessUrl("/logout-success").permitAll();
+//	}
+	
+	// Using OAuth Security
+	protected void configure(HttpSecurity http) throws Exception{
+		http 
+			.csrf().disable()
+			.authorizeRequests().requestMatchers("/login").permitAll()
+			.anyRequest().authenticated();
+	}
+	
+	
+	// this method is used for security with a plain text password
+//	@Bean
+//	@Override
+//	protected UserDetailsService userDetailsService() {
+//		List<UserDetails> users = new ArrayList<>();
+//		users.add(User.withDefaultPasswordEncoder().username("devanshu").password("123qwe,./").roles("USER").build());
+//		
+//		return new InMemoryUserDetailsManager(users);
+//	}
+	
+}
---
+++ springSecurity/src/main/java/com/example/devanshu/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.devanshu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+	
+	@Autowired 
+	private UserRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		
+
+		
+		User user = repo.findByUsername(username);
+		if(user==null) {
+			throw new UsernameNotFoundException("User 404");
+		}
+		
+		return new UserPrincipal(user);
+	}
+
+}
